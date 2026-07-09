65d92c6435593d588df9be517b2b544c5cb343a0
Ibrahim9294 <ibrahimillia34@gmail.com>
2026-07-09 01:10:02 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,30 +4,37 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import {
+    getFirestore,
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
+import {
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
 
+// Configuration Firebase
 const firebaseConfig = {
-
-    apiKey: "VOTRE_API_KEY",
-
-    authDomain: "VOTRE_PROJET.firebaseapp.com",
-
-    projectId: "VOTRE_PROJECT_ID",
-
-    storageBucket: "VOTRE_PROJET.appspot.com",
-
-    messagingSenderId: "VOTRE_SENDER_ID",
-
-    appId: "VOTRE_APP_ID"
-
+    apiKey: "AIzaSyBfeVEi48HzJEsv2V8IINeOKutVF40YT7Q",
+    authDomain: "entreprise-arashi.firebaseapp.com",
+    projectId: "entreprise-arashi",
+    storageBucket: "entreprise-arashi.firebasestorage.app",
+    messagingSenderId: "468316551281",
+    appId: "1:468316551281:web:1827614fe1ad32e60ac3d0",
+    measurementId: "G-KCGS3DXJ5L"
 };
 
+// Initialisation Firebase
 const app = initializeApp(firebaseConfig);
 
+// Base de données
 const db = getFirestore(app);
 
+// Stockage
 const storage = getStorage(app);
 
 // Rendre disponibles dans script.js
@@ -41,3 +48,5 @@ window.firebaseFunctions = {
     uploadBytes,
     getDownloadURL
 };
+
+console.log("✅ Firebase ARASHI connecté");
